e25499488a0c78792d0a994cd65fc4b77278aa86
Christopher II Lajom <christopheriilajom@gmail.com>
2021-08-01 21:53:56 +0800

    Edited recipe item component.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,6 +10,7 @@ export class RecipeItemComponent implements OnInit {
   // @Input('recipeName') recipeName: string = '';
   // @Input('description') description: string = '';
   // @Input('imagePath') imagePath: string = '';
+  @Input('recipe') recipe: Recipe;
 
   constructor() {}
 
